e1971b312b629a27600245427fa856a87fd7878c
Daniel Ciochiu <daniel.ciochiu@gmail.com>
2025-04-26 15:22:33 +0300

    Updated React build to exclude react and react-dom from bundling

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,9 +1,21 @@
 // @ts-ignore
 import { viteObfuscateFile } from 'vite-plugin-obfuscator';
 import { defineConfig } from 'vite';
+import { viteExternalsPlugin } from 'vite-plugin-externals';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            // react: 'https://unpkg.com/react@18/umd/react.production.min.js',
+            // 'react-dom': 'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'
+        }
+    },
     plugins: [
+        viteExternalsPlugin({
+            react: 'React',
+            'react-dom': 'ReactDOM',
+            'react-dom/client': 'ReactDOM'
+        }),
         viteObfuscateFile({
             deadCodeInjection: true,
             deadCodeInjectionThreshold: 0.4,
@@ -14,13 +26,17 @@ export default defineConfig({
     ],
     build: {
         rollupOptions: {
-            external: ['react', 'react-dom'],
+            external: ['react', 'react-dom', 'react-dom/client'],
             output: {
                 globals: {
                     react: 'React',
-                    'react-dom': 'ReactDOM'
+                    'react-dom': 'ReactDOM',
+                    'react-dom/client': 'ReactDOM'
                 }
             }
         }
+    },
+    optimizeDeps: {
+        exclude: ['react', 'react-dom', 'react-dom/client']
     }
 });
